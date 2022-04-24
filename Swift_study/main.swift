f550633ee44f9a9d7df61397d35113b674497fce
UnGwan <jkj8842@gmail.com>
2022-04-24 22:54:44 +0900

    2020-04-24 swift study start

--- Swift_study/main.swift
+++ Swift_study/main.swift
@@ -1,11 +1,51 @@
-//
-//  main.swift
-//  Swift_study
-//
-//  Created by 정운관 on 2022/04/24.
-//
+// 기본 데이터 타입
 
 import Foundation
 
-print("Hello, World!")
+
+//타입 종류:Bool,Int,UInt,Float,Double,CharacterSet,String
+
+//bool
+
+var someBool:Bool = true
+someBool = false
+//someBool = 0 not integer type
+//someBool = 1
+
+//int
+
+var someInt :Int = -100
+
+//UInt -> unsingend
+
+var someUint :UInt=100 //NO -
+
+//float
+
+var someFloat : Float=3.14
+someFloat=3 // no error
+
+
+//double
+
+var someDouble : Double = 3.14
+someDouble=3 // no error
+//someDouble=someFloat //error
+
+//character
+
+var someChar :Character = "d" // unicode, only one char
+print(someChar)
+
+
+//String
+
+var someString : String = "하하하하핳"
+someString = someString+"gigigigigi"
+print(someString)
+
+//someString = someChar //error
+
+// 스위프트에서는 다른 자료형간의 교환은 안되고 따로 변환해주는 방법이 있다.
+
 
---
+++ Swift_study/변수와 상수.swift
@@ -0,0 +1,31 @@
+// 변수와 상수
+
+import Foundation
+
+//상수 변수의 선언
+
+//상수 let
+//변수 var
+
+let constant:String = "sdsds"
+var variable:String = "sdsdsdsd"
+
+// constant = "asdasdasd"  error
+variable = "1234"
+
+
+//상수 선언 후에 나중에 값 할당
+
+// 나중에 할당하려고 하는 상수나 변수는 타입을 꼭 명시
+let sum:Int
+let inputA: Int=100
+let inputB:Int=200
+
+sum=inputA+inputB
+//sum은 이제 변경이 불가
+
+//변수. 역시 차후에 할 당 가능
+var nickName:String
+
+nickName = "ungwan"
+nickName="guy"
